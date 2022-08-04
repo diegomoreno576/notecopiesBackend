7dabc0af129b7b41619e4e07cae021cac57f19bf
diego576 <diegomoreno576@gmail.com>
2022-08-04 23:09:14 +0200

    blogs id

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -3,9 +3,14 @@ class AuthenticationController < ApplicationController
     #POST /auth/login
     def login
        @user = User.find_by_email(params[:email])
+       @blog = Blog.where(user_id: @user.id)
        if @user&.authenticate(params[:password])
           token = jwt_encode(user_id: @user.id)
-          render json: { jwt: token }, status: :ok
+          render json: {
+             jwt: token,
+             user: @user,
+             blog: @blog
+              }, status: :ok
        else
           render json: { error: 'Unauthorized' }, status: :unauthorized
        end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
     require "securerandom"
     has_secure_password
-    has_many :users
     validates :email, presence: true, uniqueness: true
     validates :password, presence: true
     validates :username, presence: true, uniqueness: true
